6d26adc3364e79ed44c4e153dcef4fd2a818ce90
Ramazan KÜÇÜKKOÇ <ramazankucukkoc43@gmail.com>
2023-11-19 21:14:16 +0300

    düzenlendi
    düzenlendi

--- Domain/Entities/Category.cs
+++ Domain/Entities/Category.cs
@@ -4,6 +4,10 @@ namespace Domain.Entities
 {
     public class Category : Entity
     {
-     
+        public ParentCategory ParentCategory { get; set; }
+        public int ParentId { get; set; }
+        public string Name { get; set; }//Unique olacak
+        public ICollection<Product> Products { get; set; }
+
     }
 }
